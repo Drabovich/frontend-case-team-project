4e274a2fa90d8ebae4c508e599d0dc9a2a751319
Galina <galina.mckoy@gmail.com>
2023-04-30 20:51:19 +0200

    add section about-event

---
+++ js/event-page.js
@@ -0,0 +1,25 @@
+//accordition 
+
+const titles = document.querySelectorAll('.about-event__subtitle');
+const contents = document.querySelectorAll('.about-event__text');
+
+titles.forEach(item => item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tab);
+
+    if (activeContent.classList.contains('active')) {
+        activeContent.classList.remove('active');
+        item.classList.remove('active');
+        activeContent.style.maxHeight = 0;
+    } else {
+        contents.forEach(element => {
+            element.classList.remove('active');
+            element.style.maxHeight = 0;
+        });
+
+        titles.forEach(element => element.classList.remove('active'));
+
+        item.classList.add('active');
+        activeContent.classList.add('active');
+        activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+}))
