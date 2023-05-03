529cd3b4b7909034cb7f032658a3d557f9639755
Andrey <virushmirus911@gmail.com>
2023-05-03 21:25:07 +0200

    courses-corrected

--- js/courses.js
+++ js/courses.js
@@ -1,16 +1,16 @@
-const swiper = new Swiper('.swiper', {
-	loop: true,
+// const swiper = new Swiper('.swiper', {
+// 	loop: true,
 
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true
-	},
-
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
 	
-});
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		clickable: true
+// 	},
+
+	
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+	
+// });
