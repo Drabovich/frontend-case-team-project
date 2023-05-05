e741cbf06473c96b177d308aaeda298e2dd3ff5d
xalenan <alena5nazarova@gmail.com>
2023-05-05 13:53:11 +0300

    add section reviews

--- js/main-page.js
+++ js/main-page.js
@@ -40,3 +40,37 @@ new Swiper('.team-slider', {
         }        
     },
 });
+
+
+new Swiper('.reviews-slider', {
+    navigation: {
+        nextEl: '.reviews-swiper-button-next',
+        prevEl: '.reviews-swiper-button-prev'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+
+    simulateTouch: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    grabCursor: true,
+    slideToClickedSlide: true,
+    autoplay: true,
+    
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true
+    },
+    
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    initialSlide: 0,
+    speed: 900,
+    loop: true,
+});
